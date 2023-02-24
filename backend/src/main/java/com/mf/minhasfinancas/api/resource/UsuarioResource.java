2909c25d63f49cd6bd5207b495aaef7894a44df5
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2023-02-24 02:54:41 -0300

    Implements autenticar method controller and the /api/usuarios/autenticar endPoint

--- backend/src/main/java/com/mf/minhasfinancas/api/resource/UsuarioResource.java
+++ backend/src/main/java/com/mf/minhasfinancas/api/resource/UsuarioResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mf.minhasfinancas.api.dto.UsuarioDTO;
+import com.mf.minhasfinancas.exception.ErroAutenticacao;
 import com.mf.minhasfinancas.exception.RegraNegocioException;
 import com.mf.minhasfinancas.model.entity.Usuario;
 import com.mf.minhasfinancas.service.UsuarioService;
@@ -21,9 +22,24 @@ public class UsuarioResource {
 	@Autowired
 	private UsuarioService service;
 	
-	
+	//Construtor
 	public UsuarioResource(UsuarioService service) {
 		this.service = service;
+	}
+
+	
+	@PostMapping(value = "/autenticar")
+	public ResponseEntity autenticar(@RequestBody UsuarioDTO usuarioDto) {
+			
+		try {
+			Usuario usuarioAutenticado = service.autenticar(usuarioDto.getEmail(), usuarioDto.getSenha());
+			
+			return new ResponseEntity(usuarioAutenticado, HttpStatus.OK);
+			
+		} catch (ErroAutenticacao e) {
+			
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
 	
 
